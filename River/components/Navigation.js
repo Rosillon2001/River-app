722488933a9fdc2b83b581e7e514e7972f3b325b
rosillon <rosillon2001@gmail.com>
2021-11-19 19:24:45 -0400

    Added Tab Navigation

---
+++ River/components/Navigation.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+
+import HomeScreen from "../screens/HomeScreen";
+import ExploreScreen from "../screens/ExploreScreen";
+import ProfileScreen from "../screens/ProfileScreen";
+
+const tabNavigator = createBottomTabNavigator();
+
+export default function Navigation(){
+    return(
+        <NavigationContainer>
+            <tabNavigator.Navigator
+                screenOptions = {({ route }) => ({
+                    tabBarIcon:({ focused, color, size}) =>{
+                        let iconName;
+                        if(route.name === 'Home') {
+                            iconName = focused ? 'home' : 'home-outline'
+                        }
+                        if(route.name === 'Explore') {
+                            iconName = focused ? 'search' : 'search-outline'
+                        }
+                        if(route.name === 'Profile') {
+                            iconName = focused ? 'person' : 'person-outline'
+                        }
+                        return <Ionicons name={iconName} size={size} color={color}/>
+                    }
+                })
+                }>
+                <tabNavigator.Screen name="Home" component = {HomeScreen}/>
+                <tabNavigator.Screen name="Explore" component = {ExploreScreen}/>
+                <tabNavigator.Screen name="Profile" component = {ProfileScreen}/>
+            </tabNavigator.Navigator>
+        </NavigationContainer>
+    )
+}
