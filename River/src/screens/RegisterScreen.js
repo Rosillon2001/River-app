4643a3b84d9180edd9a687e7983551a77c9ea590
JoseAIG <inciartegj@gmail.com>
2021-11-21 14:03:02 -0400

    Profile picture upload
    Register user with or without profile images.
    Created ImageInput component.

--- River/src/screens/RegisterScreen.js
+++ River/src/screens/RegisterScreen.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { Text, View, TextInput, TouchableOpacity, Picker, StyleSheet } from 'react-native';
+import { ScrollView, Text, View, TextInput, TouchableOpacity, Button, StyleSheet, Platform, Image } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import Loading from "../components/Loading";
 import LocationPicker from "../components/LocationPicker";
+import ImageInput from "../components/ImageInput";
 import axios from "axios";
 
 export default function RegisterScreen() {
@@ -20,6 +21,7 @@ export default function RegisterScreen() {
     const [name, setName] = useState("");
     const [bio, setBio] = useState("");
     const [location, setLocation] = useState("");
+    const [picture, setPicture] = useState(null);
 
     // USE EFFECT FOR ENABLING/DISABLING REGISTER BUTTON
     useEffect(() => {
@@ -43,6 +45,8 @@ export default function RegisterScreen() {
     }
 
     const register = () => {
+        setLoading(true)
+
         let formData = new FormData()
         formData.append('username', username)
         formData.append('email', email)
@@ -52,69 +56,82 @@ export default function RegisterScreen() {
         formData.append('location', location)
         formData.append('birthDate', "")
 
+        if (picture && Platform.OS !== 'web') {
+            let fileType = picture.substring(picture.lastIndexOf(".") + 1);
+            formData.append('picture', { uri: picture, name: `photo.${fileType}`, type: `image/${fileType}` })
+        }
 
         axios.post("https://app-river.herokuapp.com/register", formData)
             .then(async (response) => {
                 console.log(response.data);
+                setLoading(false)
             })
             .catch(error => {
                 console.log(error.response.data);
+                setLoading(false)
             });
     }
 
     return (
-        <View style={styles.inputView}>
-            <Text style={styles.title}>Register</Text>
-            <TextInput
-                style={styles.inputText}
-                textContentType="username"
-                placeholder="Username"
-                placeholderTextColor="gray"
-                onChangeText={text => setUsername(text)}
-            />
-            <TextInput
-                style={styles.inputText}
-                textContentType="emailAddress"
-                placeholder="Email"
-                placeholderTextColor="gray"
-                onChangeText={text => validateEmail(text)}
-            />
-            <TextInput
-                style={styles.inputText}
-                textContentType="password"
-                secureTextEntry={true}
-                placeholder="Password"
-                placeholderTextColor="gray"
-                onChangeText={text => setPassword(text)}
-            />
-            <View style={{ borderBottomColor: 'gray', borderBottomWidth: 1, display: 'flex', alignItems: 'center', flexDirection: 'row' }}>
-                <Ionicons name={optionalInfo ? 'chevron-up' : 'chevron-down'} size={20} color='gray' />
-                <Text style={{ color: 'gray' }} onPress={() => { setOptionalInfo(!optionalInfo) }}>Optional info</Text>
-            </View>
-            <View style={optionalInfo ? {} : { display: 'none' }}>
+        <ScrollView>
+            <View style={styles.inputView}>
+                <Text style={styles.title}>Register</Text>
+
+                <View style={{ alignSelf: "center" }}>
+                    {picture && <Image source={{ uri: picture }} style={{ width: 200, height: 200, borderRadius: 10 }} />}
+                </View>
+
                 <TextInput
                     style={styles.inputText}
-                    textContentType="name"
-                    placeholder="Name"
+                    textContentType="username"
+                    placeholder="Username"
                     placeholderTextColor="gray"
-                    onChangeText={text => setName(text)}
+                    onChangeText={text => setUsername(text)}
                 />
                 <TextInput
                     style={styles.inputText}
-                    textContentType="none"
-                    placeholder="Bio"
+                    textContentType="emailAddress"
+                    placeholder="Email"
                     placeholderTextColor="gray"
-                    onChangeText={text => setBio(text)}
+                    onChangeText={text => validateEmail(text)}
                 />
-                <LocationPicker onLocationChange={setLocation} />
+                <TextInput
+                    style={styles.inputText}
+                    textContentType="password"
+                    secureTextEntry={true}
+                    placeholder="Password"
+                    placeholderTextColor="gray"
+                    onChangeText={text => setPassword(text)}
+                />
+                <View style={{ borderBottomColor: 'gray', borderBottomWidth: 1, display: 'flex', alignItems: 'center', flexDirection: 'row' }}>
+                    <Ionicons name={optionalInfo ? 'chevron-up' : 'chevron-down'} size={20} color='gray' />
+                    <Text style={{ color: 'gray' }} onPress={() => { setOptionalInfo(!optionalInfo) }}>Optional info</Text>
+                </View>
+                <View style={optionalInfo ? {} : { display: 'none' }}>
+                    <TextInput
+                        style={styles.inputText}
+                        textContentType="name"
+                        placeholder="Name"
+                        placeholderTextColor="gray"
+                        onChangeText={text => setName(text)}
+                    />
+                    <TextInput
+                        style={styles.inputText}
+                        textContentType="none"
+                        placeholder="Bio"
+                        placeholderTextColor="gray"
+                        onChangeText={text => setBio(text)}
+                    />
+                    <LocationPicker onLocationChange={setLocation} />
+                    <ImageInput onImageSelected={setPicture} />
+                </View>
+                <TouchableOpacity disabled={disabledRegister} style={disabledRegister ? styles.disabledButton : styles.button} onPress={register}>
+                    <Text style={{ color: "white" }}>Register</Text>
+                </TouchableOpacity>
 
+                <Loading activated={loading} />
             </View>
-            <TouchableOpacity disabled={disabledRegister} style={disabledRegister ? styles.disabledButton : styles.button} onPress={register}>
-                <Text style={{ color: "white" }}>Register</Text>
-            </TouchableOpacity>
-
-            <Loading activated={loading} />
-        </View>
+        </ScrollView>
     );
 }
 
@@ -122,6 +139,7 @@ const styles = StyleSheet.create({
     inputView: {
         alignSelf: "center",
         marginTop: 25,
+        marginBottom: 25,
         width: "80%",
         backgroundColor: "#dddddd",
         borderRadius: 25,
